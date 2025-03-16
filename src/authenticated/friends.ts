8b5ed6608d6e8fbd2c654693f000f51b93c91802
Jonasz Mielke <168871782+jonaszmielke@users.noreply.github.com>
2025-03-16 12:09:26 +0100

    Added reject friend request handler

--- src/authenticated/friends.ts
+++ src/authenticated/friends.ts
@@ -130,4 +130,34 @@ friends_router.post("/accept", async (req, res) => {
     res.status(200).json({message: 'Friend request accepted'});
 });
 
+
+friends_router.post("/reject", async (req, res) => {
+
+    const friendrequestid = parseInt(req.query.friendrequestid);
+
+    if (!friendrequestid){
+        res.status(400).json({error: `Wrong receiverid ${friendrequestid}`});
+        return;
+    }
+
+    const friendrequest = await prisma.friendRequest.findUnique({
+        where: {
+            id: friendrequestid,
+            status: 'pending'
+        }
+    });
+
+    if (!friendrequest) {
+        res.status(404).json({error: `Friend request ${friendrequestid} does not exist`});
+        return;
+    }
+
+    await prisma.friendRequest.delete({
+        where: {id: friendrequestid}
+    });
+
+    res.status(200).json({message: 'Friend request rejected'});
+});
+
+
 export default friends_router;
